0e2ea2aed1d9d039d8d841270752a557fa95cad5
taukir4u <taukir4u@gmail.com>
2014-12-26 19:14:43 +0600

    C#
    Signed-off-by: taukir4u <taukir4u@gmail.com>

---
+++ BankAccountOperationApp/Account.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountOperationApp
+{
+    class Account
+    {
+        public string accountNumber;
+        public string customerName;
+        public double balance;
+
+        public Account()
+        {
+            balance = 0;
+        }
+
+        public string deposit(double amount)
+        {
+            balance += amount;
+            return amount + " Taka Has Deposited";
+        }
+
+        public string withdraw(double amount)
+        {
+            if (balance - amount >=0)
+            {
+                balance -= amount;
+                return amount + " Taka Has Withdraw";
+            }
+            else
+            {
+                return "No Suficient Balance";
+            }
+        }
+    }
+
+    
+}
---
+++ BankAccountOperationApp/BankAccountOperationUI.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BankAccountOperationApp
+{
+    public partial class BankAccountOperationUI : Form
+    {
+
+        private Account myAccount = null;
+        private double amount;
+        
+        public BankAccountOperationUI()
+        {
+            InitializeComponent();
+        }
+
+        private void createButton_Click(object sender, EventArgs e)
+        {
+            myAccount = new Account();
+            myAccount.accountNumber = accountNumberTextBox.Text;
+            myAccount.customerName = customerNameTextBox.Text;
+            MessageBox.Show("Account has been created.\nAccount Number: " + myAccount.accountNumber + "\nCustomer Name: " +
+                                myAccount.customerName);
+        }
+
+        private void depositButton_Click(object sender, EventArgs e)
+        {
+            amount = Convert.ToDouble(transactionTextBox.Text);
+            MessageBox.Show(myAccount.deposit(amount));
+        }
+
+        private void withdrawButton_Click(object sender, EventArgs e)
+        {
+            amount = Convert.ToDouble(transactionTextBox.Text);
+            MessageBox.Show(myAccount.withdraw(amount));
+        }
+
+        private void reportButton_Click(object sender, EventArgs e)
+        {
+            if (myAccount !=null)
+            {
+                MessageBox.Show(myAccount.customerName + "Your Acc No:" + myAccount.accountNumber
+                    + "& Balance:"+ myAccount.balance +"Taka");
+            }
+        }
+    }
+}
